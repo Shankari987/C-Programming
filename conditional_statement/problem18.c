cec2abab62237ebb444befb29f3557e2be131954
Shankari987 <e23it005@shanmugha.edu.in>
2026-02-14 23:03:43 +0530

    Electricity Bill

---
+++ conditional_statement/problem18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int units,bill;
+    scanf("%d",&units);
+    if(units<=100){
+        bill=units*5;
+    }
+    else{
+        bill=(100*5)+(units-100)*10;
+    }
+ 
+    printf("%d",bill);
+}
