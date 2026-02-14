bae8509d11034712684d3d4be266ab415d84c805
Shankari987 <e23it005@shanmugha.edu.in>
2026-02-14 23:01:54 +0530

    Grade calculation

---
+++ conditional_statement/problem12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int marks;
+    scanf("%d",&marks);
+    if(marks<0 ||marks>100){
+        printf("Invalid marks");
+    }
+    else if(marks>=90&&marks<=100){
+        printf("Grade A");
+    }
+    else if (marks>=75 && marks<90){
+        printf("Grade B");
+    }
+    else if (marks>=35 && marks<75) {
+        printf("Grade C");
+    }
+    else{
+        printf("Grade D");
+    }
+}
