0c4618f28102147a43513cdc9dccab14cc1a612c
Shankari987 <e23it005@shanmugha.edu.in>
2026-02-14 22:58:28 +0530

    Check divisible by 5

---
+++ conditional_statement/problem3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n%5==0){
+        printf("%d is divisible by 5",n);
+    }
+    else{
+        printf("%d is not divisible by 5",n);
+    }
+}
