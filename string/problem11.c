d955f7ff24d7cbdb1caa0c83b91d5c463ba4e9d3
Shankari987 <e23it005@shanmugha.edu.in>
2026-02-14 22:53:30 +0530

    Digit counter

---
+++ string/problem11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    char s[30];
+    fgets(s,30,stdin);
+    int count=0;
+    int i=0;
+    while(s[i]!='\0'){
+        if(s[i]<='9'&&s[i]>='0'){
+            count++;
+        }
+        i++;
+    }
+    printf("%d",count);
+}
